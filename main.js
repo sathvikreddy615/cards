df4ea01d7d2c63b424a58f65fc53e99e4299f0a8
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-26 15:03:41 -0500

    shortened the code by getting rid of two functions. added full functionality to the colorPicker functions, instead of creating seperate functions that target the event

--- main.js
+++ main.js
@@ -1,60 +1,59 @@
 let createBtn = document.querySelector("#create-btn");
 let cardContainer = document.querySelector("#card-container");
-// 
 
+const cardCreator = () => {
 
-createBtn.addEventListener("click", () => {
-    
-    const colorPickerOne = () => {
-        let input1 = document.createElement("input");
-        input1.type = "color";
-        input1.className = "picker1";
-        input1.name = "picker1";
-        input1.addEventListener("change", changeBackgroundColor);
-        return input1;
-    }
+    createBtn.addEventListener("click", () => {
+        
+        const colorPickerOne = () => {
+            let input1 = document.createElement("input");
+            input1.type = "color";
+            input1.className = "picker1";
+            input1.name = "picker1";
+            input1.addEventListener("change", () => {
+                event.target.parentNode.style.backgroundColor = event.target.value;
+            });
+            return input1;
+        }
 
-    const changeBackgroundColor = () => {
-        event.target.parentNode.style.backgroundColor = event.target.value;
-    }
+        const colorPickerTwo = () => {
+            let input2 = document.createElement("input");
+            input2.type = "color";
+            input2.className = "picker2";
+            input2.name = "picker2";
+            input2.addEventListener("change", () => {
+                event.target.parentNode.style.color = event.target.value;
+            });
+            return input2;
+        }
 
-    const colorPickerTwo = () => {
-        let input2 = document.createElement("input");
-        input2.type = "color";
-        input2.className = "picker2";
-        input2.name = "picker2";
-        input2.addEventListener("change", changeFontColor);
-        return input2;
-    }
+        let card = document.createElement("div");
+        card.className = "card";
 
-    const changeFontColor = () => {
-        event.target.parentNode.style.color = event.target.value;
-    }
+        const deleteButton = () => {
+            let deleteBtn = document.createElement("button");
+            deleteBtn.className = "delete-btn";
+            deleteBtn.innerHTML = "Delete";
+            deleteBtn.addEventListener("click", () => {
+                event.target.parentNode.remove();
+            });
+            return deleteBtn;
+        }
 
-    let card = document.createElement("div");
-    card.className = "card";
+        const addUserText = () => {
+            let userText = document.querySelector("#text-box").value;
+            let injectText = document.createElement("p");
+            injectText.innerHTML = userText;
+            return injectText;
+        }
 
-    const deleteButton = () => {
-        let deleteBtn = document.createElement("button");
-        deleteBtn.className = "delete-btn";
-        deleteBtn.innerHTML = "Delete";
-        deleteBtn.addEventListener("click", () => {
-            event.target.parentNode.remove();
-        });
-        return deleteBtn;
-    }
+        cardContainer.appendChild(card);
+        card.appendChild(colorPickerOne());
+        card.appendChild(colorPickerTwo());
+        card.appendChild(deleteButton());
+        card.appendChild(addUserText());
 
-    const addUserText = () => {
-        let userText = document.querySelector("#text-box").value;
-        let injectText = document.createElement("p");
-        injectText.textContent = userText;
-        return injectText;
-    }
+    })
+}
 
-    cardContainer.appendChild(card);
-    card.appendChild(colorPickerOne());
-    card.appendChild(colorPickerTwo());
-    card.appendChild(deleteButton());
-    card.appendChild(addUserText());
-
-})
+cardCreator();
